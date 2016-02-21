aac038e197643dc26b5eb824a88b56f8cf582897
Simon Wears <munkyjunky@gmail.com>
2016-02-21 17:22:46 +0000

    Added a basic HTTP handler

--- generators/app/templates/src/index.js
+++ generators/app/templates/src/index.js
@@ -2,8 +2,13 @@
 
 module.exports = (robot) => {
 
-	robot.hear(/badger/i, (res)=> {
+	robot.hear(/badger/i, res => {
 		res.send('Badgers? BADGERS? WE DON\'T NEED NO STINKIN BADGERS!');
+	});
+
+	robot.router.post('/test', (req, res) => {
+		res.send('OK');
+		robot.send('Received HTTP request');
 	});
 
 };
